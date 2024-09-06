63e596155beed55c6a218d8c8911bcbfb07482b0
juan <jdeomoya@gmail.com>
2024-09-06 20:26:11 +0200

    solucionado el scroll de las denuncias

--- src/components/cardDenuncias.jsx
+++ src/components/cardDenuncias.jsx
@@ -1,14 +1,23 @@
-import { useState, useEffect } from "react";
-import Dashboard from "./dashboard";
-
-function Denuncias({ id, name }) {
+function Denuncias({ id, name, onDelete, url }) {
+  const deletePost = async () => {
+    try {
+      await fetch(`${url}${id}`, { method: "DELETE" });
+      console.log(`Deleted user with ID: ${id}`);
+      onDelete(id);
+    } catch (e) {
+      console.error("Failed to delete the user:", e);
+    }
+  };
   return (
     <div className="w-[15rem] h-80 sm:w-[14rem] sm:h-[19rem] md:w-[15rem] md:h-[19rem] xl:w-[17rem] xl:h-[20rem] 2xl:w-[18rem] 2xl:h-[20rem] relative p-5 m-5 border mx-auto border-violet-900 rounded items-center text-base hover:scale-110 transition duration-300 grid">
       <div className="w-full h-full text-base texto-pizarra overflow-hidden grid items-center">
         <span className="text-center text-2xl">{name}</span>
         <hr className="w-full mx-auto border-violet-900"></hr>
       </div>
-      <button className="text-white bg-red-500 border border-red-500 rounded-lg flex items-center justify-center h-10 px-4 hover:bg-red-600 hover:border-red-600 transition duration-300 ease-in-out">
+      <button
+        onClick={deletePost}
+        className="text-white bg-red-500 border border-red-500 rounded-lg flex items-center justify-center h-10 px-4 hover:bg-red-600 hover:border-red-600 transition duration-300 ease-in-out"
+      >
         Eliminar
       </button>
     </div>
--- src/components/denuncias.jsx
+++ src/components/denuncias.jsx
@@ -5,11 +5,7 @@ import Card from "./cardDenuncias";
 function Denuncias() {
   const [denuncias, setDenuncias] = useState([]);
   const [isSearching, setisSearching] = useState("");
-
   const url = "https://jsonplaceholder.typicode.com/users/";
-
-  // Función para capturar las denuncias
-  const handleDelete = (id) => {};
 
   const captDenuncias = async () => {
     try {
@@ -36,14 +32,32 @@ function Denuncias() {
     captDenuncias();
   }, []); // El array vacío asegura que se ejecute solo una vez al montar el componente
 
+  const handleDelete = (id) => {
+    /*Pasamos como parametro prevRestaurant para dar el valor más actualizado de el array en caso
+     * de pasar restaurants el array estaría desactualizado.
+     */
+    setDenuncias((prevDenuncias) =>
+      /*filtramos el valor actualizado de restaurants por el id que recibe como parametro y volvemos
+       * a rellenar el array inicial por restaurant con el id que no sea el eliminado para que este
+       * no pertenezca a el array inicial
+       */
+      prevDenuncias.filter((denuncia) => denuncia.id !== id)
+    );
+  };
+
   return (
     <div className="flex flex-row">
       <Dashboard />
-      <div className="items-center w-screen flex flex-wrap">
-        {/* Renderiza las denuncias si no se está buscando */}
+      <div className="items-center w-screen h-screen flex flex-wrap overflow-y-scroll">
         {isSearching === "" ? (
           denuncias.map((denuncia) => (
-            <Card key={denuncia.id} id={denuncia.id} name={denuncia.name} />
+            <Card
+              key={denuncia.id}
+              id={denuncia.id}
+              name={denuncia.name}
+              url={url}
+              onDelete={handleDelete}
+            />
           ))
         ) : (
           <p>No se encontraron resultados de búsqueda</p>
